ac80d1653d69b11b919c689c643673726e044976
noppe <noppelabs@gmail.com>
2026-06-25 17:05:16 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,15 +10,12 @@ let package = Package(
         .macOS(.v26),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DialStylePicker",
             targets: ["DialStylePicker"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DialStylePicker",
             swiftSettings: [
